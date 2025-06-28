f05cf4da66cf579ed4f8b5a7014399b77bc495ce
edu4rdolinux <esbarros@proton.me>
2025-06-27 23:36:55 -0300

    menu test

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+# MENU
+def print_menu():
+    print('Choose an option:\n')
+
+    test_list = ['Test 1', 'Test 2', 'Test 3']
+    for x in range(len(test_list)):
+        print(str(x + 1) + '. ' + test_list[x])
+
 print('<---- Welcome to PyBet ---->')
+print_menu()
