1181f4cff1ffddb0e35d1fb9aaf41ab5fac52cb8
edu4rdolinux <esbarros@proton.me>
2025-06-29 16:59:25 -0300

    switch function

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+# SWITCH
+def switch(option):
+    if option == "1":
+        return "You chose the Roulette."
+    elif option == "2":
+        return "You chose the Test."
+    else:
+        return "Invalid option."
+
 # MENU
 def print_menu():
     print('Choose an option:\n')
 
-    test_list = ['Roulette']
+    test_list = ['Roulette', 'Test']
     for x in range(len(test_list)):
-        print(str(x + 1) + '. ' + test_list[x])
+        print(f"{x + 1}. {test_list[x]}")
 
+    option = input("\nEnter your choice: ")
+    result = switch(option)
+    print(result)
+
+# RUN THE PROGRAM
 print('<---- Welcome to PyBet ---->')
 print_menu()
