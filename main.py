ac927f76dd227011f684a77cb1a4f690402a5e00
edu4rdolinux <esbarros@proton.me>
2025-06-29 00:24:19 -0300

    roulette option

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 def print_menu():
     print('Choose an option:\n')
 
-    test_list = ['Test 1', 'Test 2', 'Test 3']
+    test_list = ['Roulette']
     for x in range(len(test_list)):
         print(str(x + 1) + '. ' + test_list[x])
 
