cec870dac7664afbecdccdd91993ca0cad535cc4
edu4rdolinux <esbarros@proton.me>
2025-06-30 10:00:30 -0300

    more games

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ def switch(option):
     if option == "1":
         return "You chose the Roulette."
     elif option == "2":
-        return "You chose the Test."
+        return "You chose the Slots."
+    elif option == "3":
+        return "You chose the Rubbelkarten"
     else:
         return "Invalid option."
 
@@ -11,7 +13,7 @@ def switch(option):
 def print_menu():
     print('Choose an option:\n')
 
-    test_list = ['Roulette', 'Test']
+    test_list = ['Roulette', 'Slots', 'Rubbelkarten']
     for x in range(len(test_list)):
         print(f"{x + 1}. {test_list[x]}")
 
